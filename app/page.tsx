8b862f767889cf97fc57cee80618b9637b1ebccd
Taiyaki256 <taiyakey@mineo.jp>
2023-10-21 12:28:16 +0900

    ssr ok

--- app/page.tsx
+++ app/page.tsx
@@ -27,29 +27,33 @@ export default function Home() {
         }
     };
 
+    const wheel = () => {
+        if (typeof window !== "undefined") {
+            return (
+                <div className="wheel">
+                    <Wheel
+                        mustStartSpinning={mustSpin}
+                        prizeNumber={prizeNumber}
+                        data={data}
+                        innerRadius={17}
+                        innerBorderColor={"#000000"}
+                        innerBorderWidth={5}
+                        outerBorderWidth={7}
+                        outerBorderColor={"#000000"}
+                        //brown
+                        radiusLineColor={"#8B4513"}
+                        onStopSpinning={() => {
+                            setMustSpin(false);
+                        }}
+                    />
+                </div>
+            );
+        }
+    };
+
     return (
         <>
-            {() => {
-                if (typeof window !== "undefined") {
-                    <div className="wheel">
-                        <Wheel
-                            mustStartSpinning={mustSpin}
-                            prizeNumber={prizeNumber}
-                            data={data}
-                            innerRadius={17}
-                            innerBorderColor={"#000000"}
-                            innerBorderWidth={5}
-                            outerBorderWidth={7}
-                            outerBorderColor={"#000000"}
-                            //brown
-                            radiusLineColor={"#8B4513"}
-                            onStopSpinning={() => {
-                                setMustSpin(false);
-                            }}
-                        />
-                    </div>;
-                }
-            }}
+            {wheel()}
             <div className="spinbutton">
                 <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
