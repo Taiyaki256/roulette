29499d35aa785f794aa8a8a580e56855d50194bc
Taiyaki256 <taiyakey@mineo.jp>
2023-10-21 12:24:53 +0900

    ssr

--- app/page.tsx
+++ app/page.tsx
@@ -29,23 +29,27 @@ export default function Home() {
 
     return (
         <>
-            <div className="wheel">
-                <Wheel
-                    mustStartSpinning={mustSpin}
-                    prizeNumber={prizeNumber}
-                    data={data}
-                    innerRadius={17}
-                    innerBorderColor={"#000000"}
-                    innerBorderWidth={5}
-                    outerBorderWidth={7}
-                    outerBorderColor={"#000000"}
-                    //brown
-                    radiusLineColor={"#8B4513"}
-                    onStopSpinning={() => {
-                        setMustSpin(false);
-                    }}
-                />
-            </div>
+            {() => {
+                if (typeof window !== "undefined") {
+                    <div className="wheel">
+                        <Wheel
+                            mustStartSpinning={mustSpin}
+                            prizeNumber={prizeNumber}
+                            data={data}
+                            innerRadius={17}
+                            innerBorderColor={"#000000"}
+                            innerBorderWidth={5}
+                            outerBorderWidth={7}
+                            outerBorderColor={"#000000"}
+                            //brown
+                            radiusLineColor={"#8B4513"}
+                            onStopSpinning={() => {
+                                setMustSpin(false);
+                            }}
+                        />
+                    </div>;
+                }
+            }}
             <div className="spinbutton">
                 <button
                     className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
