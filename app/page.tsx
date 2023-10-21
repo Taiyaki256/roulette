b3dc09eb3fe748f1411750eb14494f44a0750960
Taiyaki256 <taiyakey@mineo.jp>
2023-10-21 11:21:30 +0900

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
+
+import { Wheel } from "react-custom-roulette";
+
+const data = [
+    { option: "0", style: { backgroundColor: "green", textColor: "black" } },
+    { option: "1", style: { backgroundColor: "white" } },
+    { option: "2" },
+];
+
+export default function Home() {
+    const [mustSpin, setMustSpin] = useState(false);
+    const [prizeNumber, setPrizeNumber] = useState(0);
+
+    const handleSpinClick = () => {
+        if (!mustSpin) {
+            const newPrizeNumber = Math.floor(Math.random() * data.length);
+            setPrizeNumber(newPrizeNumber);
+            setMustSpin(true);
+        }
+    };
+
+    return (
+        <>
+            <Wheel
+                mustStartSpinning={mustSpin}
+                prizeNumber={prizeNumber}
+                data={data}
+                onStopSpinning={() => {
+                    setMustSpin(false);
+                }}
+            />
+            <button onClick={handleSpinClick}>SPIN</button>
+        </>
+    );
+}
