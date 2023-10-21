3439c3673e43e6a97cf32ba099816c6eca9020a8
Taiyaki256 <taiyakey@mineo.jp>
2023-10-21 13:55:56 +0900

    upd

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import React, { useState } from "react";
 import dynamic from "next/dynamic";
 
 const Wheel = dynamic(() => import("./wheel"), { ssr: false });
-
 const data = [
     { option: "0", style: { backgroundColor: "green", textColor: "white" } },
     { option: "1", style: { backgroundColor: "red", textColor: "white" } },
@@ -15,6 +14,34 @@ const data = [
     { option: "6", style: { backgroundColor: "black", textColor: "white" } },
     { option: "7", style: { backgroundColor: "red", textColor: "white" } },
     { option: "8", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "9", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "10", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "11", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "12", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "13", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "14", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "15", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "16", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "17", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "18", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "19", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "20", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "21", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "22", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "23", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "24", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "25", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "26", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "27", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "28", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "29", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "30", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "31", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "32", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "33", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "34", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "35", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "36", style: { backgroundColor: "black", textColor: "white" } },
 ];
 
 export default function Home() {
--- app/wheel.tsx
+++ app/wheel.tsx
@@ -14,6 +14,34 @@ const data = [
     { option: "6", style: { backgroundColor: "black", textColor: "white" } },
     { option: "7", style: { backgroundColor: "red", textColor: "white" } },
     { option: "8", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "9", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "10", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "11", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "12", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "13", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "14", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "15", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "16", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "17", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "18", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "19", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "20", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "21", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "22", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "23", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "24", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "25", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "26", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "27", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "28", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "29", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "30", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "31", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "32", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "33", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "34", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "35", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "36", style: { backgroundColor: "black", textColor: "white" } },
 ];
 type HeadingProps = {
     mustSpin: boolean;
@@ -27,13 +55,17 @@ const clientOnly = ({ mustSpin, prizeNumber, setMS }: HeadingProps) => {
                 mustStartSpinning={mustSpin}
                 prizeNumber={prizeNumber}
                 data={data}
-                innerRadius={17}
+                innerRadius={55}
                 innerBorderColor={"#000000"}
-                innerBorderWidth={5}
-                outerBorderWidth={7}
+                innerBorderWidth={7}
+                outerBorderWidth={5}
                 outerBorderColor={"#000000"}
+                fontSize={13}
+                textDistance={75}
+                perpendicularText={true}
                 //brown
                 radiusLineColor={"#8B4513"}
+                radiusLineWidth={3}
                 onStopSpinning={setMS}
             />
         </div>
