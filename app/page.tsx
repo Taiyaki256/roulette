5ff318c00a95d15cd7d29ff439f5b8e7567dec5a
Taiyaki256 <taiyakey@mineo.jp>
2023-10-21 12:04:59 +0900

    color

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,15 @@ import Image from "next/image";
 import { Wheel } from "react-custom-roulette";
 
 const data = [
-    { option: "0", style: { backgroundColor: "green", textColor: "black" } },
-    { option: "1", style: { backgroundColor: "white" } },
-    { option: "2" },
+    { option: "0", style: { backgroundColor: "green", textColor: "white" } },
+    { option: "1", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "2", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "3", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "4", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "5", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "6", style: { backgroundColor: "black", textColor: "white" } },
+    { option: "7", style: { backgroundColor: "red", textColor: "white" } },
+    { option: "8", style: { backgroundColor: "black", textColor: "white" } },
 ];
 
 export default function Home() {
@@ -29,11 +35,25 @@ export default function Home() {
                     mustStartSpinning={mustSpin}
                     prizeNumber={prizeNumber}
                     data={data}
+                    innerRadius={17}
+                    innerBorderColor={"#000000"}
+                    innerBorderWidth={5}
+                    outerBorderWidth={7}
+                    outerBorderColor={"#000000"}
+                    //brown
+                    radiusLineColor={"#8B4513"}
                     onStopSpinning={() => {
                         setMustSpin(false);
                     }}
                 />
-                <button onClick={handleSpinClick}>SPIN</button>
+            </div>
+            <div className="spinbutton">
+                <button
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                    onClick={handleSpinClick}
+                >
+                    SPIN
+                </button>
             </div>
         </>
     );
